c371355fc2154acb4a473a6c6de19d0450699d03
David Boedigheimer <stealth259@gmail.com>
2020-02-27 16:01:23 -0800

    Add first test and fix color show for test.

--- __tests__/pig-dice.test.js
+++ __tests__/pig-dice.test.js
@@ -1,1 +1,15 @@
 // use "debugger" to look for bugs and broken code.
+import { Dice } from './../src/pig-dice.js';
+
+describe('Dice', () => {
+  
+  test('should roll a random number between 1-6', () => {
+    var currentRoll = new Dice(1, 2, 3, 4, 5, 6);
+    expect(currentRoll.side1).toEqual(1);
+    expect(currentRoll.side2).toEqual(2);
+    expect(currentRoll.side3).toEqual(3);
+    expect(currentRoll.side4).toEqual(4);
+    expect(currentRoll.side5).toEqual(5);
+    expect(currentRoll.side6).toEqual(6);
+  });
+});
--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { nameOfFunction } from './';
+import { Dice } from './pig-dice.js';
