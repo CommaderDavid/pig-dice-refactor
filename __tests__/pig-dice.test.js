d51714e159d1b988af96902f851de12970f4c4c8
David Boedigheimer <stealth259@gmail.com>
2020-03-02 10:34:32 -0800

    Add test for finding out if the dice rolls between a 1 or a 6.

--- __tests__/pig-dice.test.js
+++ __tests__/pig-dice.test.js
@@ -2,14 +2,10 @@
 import { Dice } from './../src/pig-dice.js';
 
 describe('Dice', () => {
-  
+
   test('should roll a random number between 1-6', () => {
-    var currentRoll = new Dice(1, 2, 3, 4, 5, 6);
-    expect(currentRoll.side1).toEqual(1);
-    expect(currentRoll.side2).toEqual(2);
-    expect(currentRoll.side3).toEqual(3);
-    expect(currentRoll.side4).toEqual(4);
-    expect(currentRoll.side5).toEqual(5);
-    expect(currentRoll.side6).toEqual(6);
+    var currentRoll = new Dice();
+    expect(currentRoll.rollDice()).toBeGreaterThanOrEqual(1);
+    expect(currentRoll.rollDice()).toBeLessThanOrEqual(6);
   });
 });
--- src/pig-dice.js
+++ src/pig-dice.js
@@ -1,8 +1,8 @@
-export function Dice(side1, side2, side3, side4, side5, side6) {
-  this.side1 = side1;
-  this.side2 = side2;
-  this.side3 = side3;
-  this.side4 = side4;
-  this.side5 = side5;
-  this.side6 = side6;
+export function Dice() {
+  this.number;
+};
+
+Dice.prototype.rollDice = function () {
+  var side = Math.floor(Math.random() * 6) + 1;
+  return side;
 };
